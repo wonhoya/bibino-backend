f1b08a38e4114ae2a5406b465222dbf434732e09
yobe8 <sanyobe@gmail.com>
2021-04-24 16:16:57 +0900

    Add error handling

--- routes/api/users/controller.js
+++ routes/api/users/controller.js
@@ -10,29 +10,36 @@ const { bibinoPrivateKey } = require("../../../config");
 
 const signInUser = async (req, res, next) => {
   const authorization = req.get("authorization");
+  let userName, userEmail, userProfileImagePath;
 
   if (authorization?.startsWith("Bearer ")) {
     try {
       const idTokenByGoogle = getidToken(authorization);
-      const {
-        name,
-        email,
-        picture: userProfileImagePath,
-      } = await authenticateUser(idTokenByGoogle);
+      const userData = await authenticateUser(idTokenByGoogle);
+      userName = userData.name;
+      userEmail = userData.email;
+      userProfileImagePath = userData.picture;
+    } catch (err) {
+      return next(createError(401, err));
+    }
+
+    try {
       const user = await User.findOneAndUpdate(
-        { email },
-        { name, email, imagePath: userProfileImagePath },
+        { email: userEmail },
+        { name: userName, imagePath: userProfileImagePath },
         { upsert: true, lean: true, new: true }
       );
 
       const idTokenByBibino = jwt.sign(user._id.toString(), bibinoPrivateKey);
       // 맥주 추천으로 가져오는 로직. 그런데 처음 가입했을 때는 유저 선호 점수가 평균 점수라 그냥 랜덤으로 가져오게 해야 하려나...
 
-      const beers = await Beer.find({}).lean();
+      const beers = await Beer.find().lean();
       res.json({ user, idTokenByBibino, beers });
     } catch (err) {
       next(createError(500, err));
     }
+  } else {
+    next(createError(401));
   }
 };
 
